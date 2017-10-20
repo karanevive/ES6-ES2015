9f983c2f370e97a67c44f8a724f95e4c7c60f8fb
karanevive <karan.arora@evivehealth.com>
2017-10-20 20:07:27 +0000

    Update 02_let_const_(a).js

--- 02_let_const_(a).js
+++ 02_let_const_(a).js
@@ -10,3 +10,29 @@ function testVar(){
   }
   console.log(a);
 }
+
+testvar(); # calling it!!!
+
+<!--
+  Output:
+  50
+  50
+    -->
+
+
+function testLet(){
+  let a = 30;  # used let
+  if(true){
+    let a = 50;  # used let
+    console.log(a); #50
+  }
+  console.log(a);  #30
+}
+
+testLet();
+
+<!--
+  Output:
+  50
+  30
+    -->
